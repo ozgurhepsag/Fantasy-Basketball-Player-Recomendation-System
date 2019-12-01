4b548a733b0e10e658464f92a8911ee66a62888b
cagdas <cagdas.bilecen@ceng.deu.edu.tr>
2019-12-02 02:13:12 +0300

    Loading spinner problem has been solved.
    There was a problem that while processes running in a new thread in signin class, progressbar spinner was not able to use (application was stopped). Problem solved with progressbar functions used in a Uithread.

---
+++ Blue&White/app/src/main/java/com/example/kamran/bluewhite/UserInfo.java
@@ -0,0 +1,57 @@
+package com.example.kamran.bluewhite;
+
+import java.util.ArrayList;
+
+import android.app.Application;
+public class UserInfo {
+    private String userName;
+    private String teamID;
+    private String teamName;
+    private String leagueID;
+    public ArrayList<Player> roster;
+    private String numberOfTeaminLeague;
+
+    public String getUserName() { return userName;}
+
+    public void setUserName(String uname) { this.userName = uname;}
+
+    public String getTeamID() {
+        return teamID;
+    }
+
+    public void setTeamID(String teamID) {
+        this.teamID = teamID;
+    }
+
+    public String getTeamName() {
+        return teamName;
+    }
+
+    public void setTeamName(String teamName) {
+        this.teamName = teamName;
+    }
+
+    public String getLeagueID() {
+        return leagueID;
+    }
+
+    public void setLeagueID(String leagueKey) {
+        this.leagueID = leagueKey;
+    }
+
+    public int getNumberOfTeaminLeague() {
+        return Integer.parseInt(numberOfTeaminLeague);
+    }
+
+    public void setNumberOfTeaminLeague(String numberOfTeaminLeague) {
+        this.numberOfTeaminLeague = numberOfTeaminLeague;
+    }
+
+    public ArrayList<Player> getRoster() {
+        return roster;
+    }
+
+    public void setRoster(ArrayList<Player> roster) {
+        this.roster = roster;
+    }
+}
